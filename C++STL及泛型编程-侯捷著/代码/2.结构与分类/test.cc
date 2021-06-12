b0ea0c04d64db86cb513c9ef7ccb0fe51c206b8f
ColderWANG <371774232@qq.com>
2021-06-12 21:45:32 +0800

    update 2021/6/12

--- C++STL及泛型编程-侯捷著/代码/2.结构与分类/test.cc
+++ C++STL及泛型编程-侯捷著/代码/2.结构与分类/test.cc
@@ -5,11 +5,14 @@
 #include <cstdio> //snprintf()
 #include <ctime>
 #include <algorithm> //sort()
+#include <map>
+
 using std::endl;
 using std::cin;
 using std::cout;
 using std::string; 
 using std::vector;
+using std::multimap;
 long get_a_target_long(){
 long target = 0;
 
@@ -47,8 +50,7 @@ namespace jj02{
                 c.push_back(string(buf));
             }
             catch(std::exception& p){
-                cout << "i=" << i << " " << p.what() << endl;
-                        
+                cout << "i=" << i << " " << p.what() << endl;     
                 abort();
             }
         }
@@ -60,15 +62,15 @@ namespace jj02{
         cout << "vector.capacity = " << c.capacity() << endl;
     
     string target = get_a_target_string();
-        {
+    {
         timeStart = clock();
     auto pItem = std::find(c.begin(),c.end(),target);  //循序查找
         cout << "std::find(), micro-seconds : " << (clock()-timeStart) << endl;          //一个clock大约是1us
         cout << "std::find(), seconds : " << (double)(clock()-timeStart)/CLOCKS_PER_SEC << endl;
         if(pItem != c.end()) cout << "found, " << *pItem << endl;
         else cout << "not found!" << endl;
-        }
-        {
+    }
+    {
         timeStart = clock();
         sort(c.begin(),c.end());
     string* pItem = (string*)bsearch(&target,(c.data()),c.size(),sizeof(string),compareStrings);  //二分查找
@@ -76,12 +78,40 @@ namespace jj02{
         cout << "sort() + bsearch(), seconds : " << (double)(clock()-timeStart)/CLOCKS_PER_SEC << endl;
         if(pItem != NULL)cout << "found, " << *pItem << endl;
         else cout << "not found!" << endl;
-        }
     }
+    }
+    void test_multimap(long& value){
+        cout << "\ntest_multimap()...............\n";
+    multimap<long,string> c;
+    char buf[10];
     
+    clock_t timeStart =clock();
+        for(long i = 0; i < value; ++i){
+            try{
+                snprintf(buf,10,"%d",rand()/10000);
+                //multimap 不可以使用[]做insertion
+                c.insert(std::pair<long,string>(i,buf));    //pair函数组成对
+            }
+            catch(std::exception& p){
+                cout << "i=" << i << " " << p.what() << endl;     
+                abort();
+            }
+        }
+        cout << "micro-seconds : " << (clock()-timeStart) << endl;
+        cout << "multimap.size() = " << c.size() <<endl;
+        cout << "multimap.max_size() = " << c.max_size() << endl;
+
+    long target = get_a_target_long();
+        timeStart = clock();
+    auto pItem = c.find(target);
+        cout << "c.find(), micro-seconds : " << (clock()-timeStart) << endl;
+        if(pItem != c.end()) cout << "found, value= " << (*pItem).second << endl;  //(*pItem).second 取出 pair 的第二个元素
+        else cout << "not found! " << endl;
+    }
 }
 
 int main(){
     long value = 1000000;
-    jj02::test_vector(value);
+    //jj02::test_vector(value);
+    jj02::test_multimap(value);
 }
